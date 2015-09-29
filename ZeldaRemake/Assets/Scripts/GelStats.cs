512faa9e642126b569484bda2e35fc62107f0d6a
Lucas Katt <kattlucas1@gmail.com>
2015-09-28 21:31:46 -0400

    improved gel movement

--- ZeldaRemake/Assets/Scripts/GelStats.cs
+++ ZeldaRemake/Assets/Scripts/GelStats.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class GelStats : EnemyStats {
   public int maxHealth = 1;
@@ -7,38 +8,52 @@ public class GelStats : EnemyStats {
   public float velocityFactor = 1.0f;
   public float chanceToChangeDirection = 0.02f;
   private float direction;
-  public float movementPeriod = 0.5f;
+  public float movementTime = 0.5f;
   private float timePassed = 0f;
   private bool waiting = false;
+	public float waitTime = 0.75f;
 
   // Use this for initialization
   void Start () {
-    direction = Random.value;
+    direction = UnityEngine.Random.value;
     changeDirection();
   }
-	
+
 	// Update is called once per frame
-	void Update () {
-    timePassed += Time.deltaTime;
-    if (timePassed >= movementPeriod)
-    {
-      if (waiting)
-      {
-        if (Random.value < chanceToChangeDirection)
-        {
-          direction = Random.value;
-        }
-        changeDirection();
-        waiting = false;
-      }
-      else
-      {
-        GetComponent<Rigidbody>().velocity = Vector3.zero;
-        waiting = true;
-      }
-      timePassed = 0;
-    }
+	void Update()
+	{
+		timePassed += Time.deltaTime;
+		if (timePassed >= movementTime && !waiting) {
+			GetComponent<Rigidbody>().velocity = Vector3.zero;
+			waiting = true;
+			timePassed = 0;
+		}
+		else if (timePassed >= waitTime && waiting)
+		{
+			waiting = false;
+			timePassed = 0;
+			if (UnityEngine.Random.value <= chanceToChangeDirection)
+				direction = UnityEngine.Random.value;
+      alignWithGrid();
+			changeDirection();
+		}
 	}
+	/*
+	void FixedUpdate() {
+		if (!waiting)
+		{
+			Vector3 newPos = transform.position;
+			float xOffset = newPos.x % 1f;
+			float yOffset = newPos.y % 1f;
+			if (UnityEngine.Random.value <= chanceToChangeDirection && ((xOffset == 0 && (yOffset > 0.95 || yOffset < 0.05)) || (yOffset == 0 && (xOffset > 0.95 || xOffset < 0.05))))
+			{
+				direction = UnityEngine.Random.value;
+				alignWithGrid();
+				changeDirection();
+			}
+		}
+	}
+	*/
 
 
   void OnTriggerEnter(Collider coll)
@@ -53,8 +68,9 @@ public class GelStats : EnemyStats {
 		}
     else if (coll.gameObject.tag == "block" || coll.gameObject.tag == "Wall" || coll.gameObject.tag == "Lock" || coll.gameObject.tag == "UpDoor" || coll.gameObject.tag == "RightDoor" || coll.gameObject.tag == "LeftDoor" || coll.gameObject.tag == "DownDoor")
     {
-      direction = (direction + 0.25f) % 1;
-      changeDirection();
+			alignWithGrid();
+			direction = (direction + 0.25f) % 1;
+			changeDirection();
     }
   }
 
@@ -79,4 +95,32 @@ public class GelStats : EnemyStats {
       GetComponent<Rigidbody>().velocity = new Vector3(-1, 0, 0) * velocityFactor;
   }
 
+	void alignWithGrid()
+	{
+		Vector3 newPos = transform.position;
+		float xOffset = newPos.x % 1f;
+		float yOffset = newPos.y % 1f;
+		double deciY = newPos.y - Math.Truncate(newPos.y);
+		double deciX = newPos.x - Math.Truncate(newPos.x);
+		if (deciY <= 0.5)
+		{
+			newPos.y -= yOffset;
+		}
+		else
+		{
+			newPos.y += (1f - yOffset);
+		}
+
+
+		if (deciX <= 0.5)
+		{
+			newPos.x -= xOffset;
+		}
+		else
+		{
+			newPos.x += (1f - xOffset);
+		}
+
+		transform.position = newPos;
+	}
 }
