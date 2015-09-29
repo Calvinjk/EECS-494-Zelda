ff792774a73f2a61b8d404ab623d366a05c9c485
Calvin Kopitz <calvinjk@umiGotmilk21ch.edu>
2015-09-29 17:42:11 -0400

    First stages of a HUD.   Toggles on and off, nothing connected yet

---
+++ ZeldaRemake/Assets/Scripts/HUD.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HUD : MonoBehaviour {
+
+    public bool _________;
+    public bool paused = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Return)) {
+            if (!paused) {
+                transform.Find("ItemSelect").GetComponent<UnityEngine.UI.Image>().enabled = true;
+                transform.Find("RupeeCount2").GetComponent<UnityEngine.UI.Text>().enabled = true;
+                transform.Find("KeyCount2").GetComponent<UnityEngine.UI.Text>().enabled = true;
+                transform.Find("BombCount2").GetComponent<UnityEngine.UI.Text>().enabled = true;
+                paused = true;
+            } else {
+                transform.Find("ItemSelect").GetComponent<UnityEngine.UI.Image>().enabled = false;
+                transform.Find("RupeeCount2").GetComponent<UnityEngine.UI.Text>().enabled = false;
+                transform.Find("KeyCount2").GetComponent<UnityEngine.UI.Text>().enabled = false;
+                transform.Find("BombCount2").GetComponent<UnityEngine.UI.Text>().enabled = false;
+                paused = false;
+            }
+        }
+    }
+}
