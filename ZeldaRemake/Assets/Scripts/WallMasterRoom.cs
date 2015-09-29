6a76c3eaaa1062c58eafd61e0eefb3ae0af3d6b8
Lucas Katt <kattlucas1@gmail.com>
2015-09-29 01:58:10 -0400

    updated wall master room not working

---
+++ ZeldaRemake/Assets/Scripts/WallMasterRoom.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class WallMasterRoom : MonoBehaviour {
+  public GameObject enemyPrefab;
+  public int numEnemies;
+  public GameObject rewardPrefab;
+  public bool cleared = false;
+  private List <GameObject> enemies;
+	public int numKilled = 0;
+
+	// Use this for initialization
+	void Start () {
+		enemies = new List<GameObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+  void OnTriggerEnter(Collider coll)
+  {
+
+  }
+
+  public void spawnEnemy(Vector3 coords, char firstDir, char secondDir)
+  {
+		if (enemies.Count < numEnemies - numKilled)
+		{
+			GameObject enemy = Instantiate(enemyPrefab, coords, Quaternion.identity) as GameObject;
+			WallMasterStats script = (WallMasterStats)enemy.GetComponent(typeof(WallMasterStats));
+			script.setRoom(this.gameObject);
+			script.firstDir = firstDir;
+			script.secondDir = secondDir;
+			enemies.Add(enemy);
+		}
+	}
+    
+  public void killedEnemy(GameObject enemy)
+  {
+    enemies.Remove(enemy);
+    Destroy(enemy);
+		++numKilled;
+    if (enemies.Count == 0)
+    {
+      cleared = true;
+      if (rewardPrefab)
+      {
+        Instantiate(rewardPrefab, this.transform.position, Quaternion.identity);
+      }
+    }
+  }
+
+	public void despawnEnemy(GameObject enemy) {
+		enemies.Remove(enemy);
+		Destroy(enemy);
+	}
+
+    void OnTriggerExit(Collider coll)
+    {
+        if (coll.gameObject.tag == "Link")
+        {
+            foreach (GameObject enemy in enemies)
+            {
+                Destroy(enemy);
+            }
+            enemies.Clear();
+        }
+    }
+}
---
+++ ZeldaRemake/Assets/Scripts/WallTriggerBottom.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallTriggerBottom : MonoBehaviour {
+	public float timeBetween = 2f;
+	private float timePassed = 0f;
+	private bool spawned = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerStay(Collider coll) {
+		if (coll.gameObject.tag == "Link") {
+			timePassed += Time.deltaTime;
+			if (!spawned) {
+				WallMasterRoom room = (WallMasterRoom)GetComponentInParent(typeof(WallMasterRoom));
+				if (coll.transform.position.x - transform.position.x > 0)
+				{
+					Vector3 spawnLocation = transform.position;
+					spawnLocation.x += 1;
+					spawnLocation.y -= 1;
+					room.spawnEnemy(spawnLocation, 'e', 'n');
+				}
+				else
+				{
+					Vector3 spawnLocation = transform.position;
+					spawnLocation.x -= 1;
+					spawnLocation.y -= 1;
+					room.spawnEnemy(spawnLocation, 'w', 'n');
+				}
+				timePassed = 0;
+				spawned = true;
+			}
+			else if (spawned && timePassed >= timeBetween) {
+				timePassed = 0;
+				spawned = false;
+			}
+		}
+
+	}
+}
