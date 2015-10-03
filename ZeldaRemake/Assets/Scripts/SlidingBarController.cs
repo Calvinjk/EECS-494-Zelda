a1a422250294439343c14105127e13e2b9d5c461
Calvin Kopitz <calvinjk@umiGotmilk21ch.edu>
2015-10-03 11:27:46 -0400

    Sliding bar trap in left1 placed.   Doesnt work yet though.  Something wrong with logic.   Just teleports to the end position right now.

---
+++ ZeldaRemake/Assets/Scripts/SlidingBarController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlidingBarController : MonoBehaviour {
+
+    public bool triggered = true;
+    public bool ____________;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    void OnTriggerEnter(Collider coll) {
+        if (coll.tag == "Link") {
+            triggered = true;
+        }
+    }
+
+    void OnTriggerExit(Collider coll) {
+        if (coll.tag == "Link") {
+            triggered = false;
+        }
+    }
+}
---
+++ ZeldaRemake/Assets/Scripts/SlidingBarTrap.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlidingBarTrap : MonoBehaviour {
+
+    public float    speed           = 0.25f;
+    public float    maxDist         = 1.5f;
+    public char     dir;
+    public bool     ____________;
+    public Vector3  startPos;
+    public Vector3  endPos;
+
+    private SlidingBarController pressurePlate;    
+
+	// Use this for initialization
+	void Start () {
+        pressurePlate = (SlidingBarController)GetComponentInParent(typeof(SlidingBarController));
+        startPos = transform.position;
+        endPos = transform.position;
+
+        //Initialize endPos
+        switch (dir) {
+            case 'n':
+                endPos.y = startPos.y + maxDist;
+                break;
+            case 's':
+                endPos.y = startPos.y - maxDist;
+                break;
+            case 'e':
+                endPos.x = startPos.x + maxDist;
+                break;
+            case 'w':
+                endPos.x = startPos.x - maxDist;
+                break;
+            default:
+                print("Error in sliding bar trap move direction");
+                break;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Move();
+	}
+
+    public void Move() {
+        //Make sure bars are not in one of two resting positions
+        if ((pressurePlate.triggered && transform.position != startPos) || (!pressurePlate.triggered && transform.position != endPos)) { 
+            switch (dir) {
+                case 'n':
+                    if (transform.position.y < endPos.y) {
+                        transform.position = new Vector3(0, transform.position.y + (0.1f * speed), 0);
+                    }
+                    else {
+                        transform.position = endPos;
+                    }
+                    break;
+                case 's':
+                    if (transform.position.y > endPos.y) {
+                        transform.position = new Vector3(0, transform.position.y - (0.1f * speed), 0);
+                    }
+                    else {
+                        transform.position = endPos;
+                    }
+                    break;
+                case 'e':
+                    //NOT IMPLEMENTED
+                    break;
+                case 'w':
+                    //NOT IMPLEMENTED
+                    break;
+                default:
+                    print("Error in sliding bar trap move direction");
+                    break;
+            }
+        }
+    }
+}
